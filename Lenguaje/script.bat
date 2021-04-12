67821334ddc9ccb30d702655a4e501104c28a392
cactaceae.cactus <cactaceae.cactus@gmail.com>
2021-04-12 17:38:57 +0200

    lexico casi

--- Lenguaje/script.bat
+++ Lenguaje/script.bat
@@ -1,4 +1,4 @@
-java -cp lib/jlex.jar JLex.Main src/AnalizadorLexicoTiny
+java -cp lib/jlex.jar JLex.Main src/alex/AnalizadorLexicoTiny
 
 java -cp lib/cup.jar java_cup.Main -parser AnalizadorSintacticoTiny -symbols ClaseLexica -nopositions src/asint/Tiny.cup
 copy AnalizadorSintacticoTiny.java src\asint\AnalizadorSintacticoTiny.java
@@ -8,7 +8,7 @@ del ClaseLexica.java
 
 cd src
 javac -cp "../lib/cup.jar" alex/*.java asint/*.java errors/*.java
-java -cp ".;../lib/cup.jar" asint/Main ../input.txt
+java -cp ".;../lib/cup.jar" asint/Main ../input3.txt
 
 cd ../
 pause
