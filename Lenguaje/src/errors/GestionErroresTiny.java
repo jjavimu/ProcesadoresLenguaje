d530d1ae9e9d1537f155ed85ce93b41109c76472
cactaceae.cactus <cactaceae.cactus@gmail.com>
2021-04-16 12:56:13 +0200

    clase errores

--- Lenguaje/src/errors/GestionErroresTiny.java
+++ Lenguaje/src/errors/GestionErroresTiny.java
@@ -3,12 +3,12 @@ package errors;
 import alex.UnidadLexica;
 
 public class GestionErroresTiny {
-   public void errorLexico(int fila, String lexema) {
-     System.out.println("ERROR fila "+fila+": Caracter inexperado: "+lexema); 
-     System.exit(1);
+   public void errorLexico(int fila, int col, String lexema) {
+     System.out.println("LEXICO ERROR fila "+fila+", columna "+col+": Caracter inexperado: "+lexema); 
+     //System.exit(1);
    }  
    public void errorSintactico(UnidadLexica unidadLexica) {
-     System.out.println("ERROR fila "+unidadLexica.fila()+": Elemento inesperado "+unidadLexica.value);
-     System.exit(1);
+     System.out.println("SINTACTICO ERROR fila "+unidadLexica.fila()+": Elemento inesperado "+unidadLexica.value);
+     //System.exit(1);
    }
 }
